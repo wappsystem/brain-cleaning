9eaa98615865febfad984863e5563c8c6911e5a0
Gunnar Unger <gunnar.unger@gmail.com>
2023-03-09 07:28:04 +1100

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -8,16 +8,16 @@
     //-------------------------------------------------------------------------------------
     m[p+"not_elig1"]         ={url:H+"/form-grid/survey/not_elig1.html"},
     m[p+"thank-you"]         ={url:H+"/form-grid/survey/thank-you.html"},
-    m[p+"survey-data"]       ={url:H+"/form-grid/survey/eligibility-data.html",Table:"backpainsleep-survey",router:1 };
-    m[p+"survey-all-data"]   ={url:H+"/form-grid/survey/eligibility-data.html",Table:"backpainsleep-survey",router:1 };
-    m[p+"contact-data"]      ={url:H+"/form-grid/survey/contact-data.html",Table:"backpainsleep-contact",form_module:"contact-form",router:1,taskname:"Contact details" };
-    m[p+"contact-form"]      ={url:H+"/form-grid/survey/contact-form.html",Table:"backpainsleep-contact"};
+    m[p+"survey-data"]       ={url:H+"/form-grid/survey/eligibility-data.html",Table:"brain-cleaning-survey",router:1 };
+    m[p+"survey-all-data"]   ={url:H+"/form-grid/survey/eligibility-data.html",Table:"brain-cleaning-survey",router:1 };
+    m[p+"contact-data"]      ={url:H+"/form-grid/survey/contact-data.html",Table:"brain-cleaning-contact",form_module:"contact-form",router:1,taskname:"Contact details" };
+    m[p+"contact-form"]      ={url:H+"/form-grid/survey/contact-form.html",Table:"brain-cleaning-contact"};
     
 
-    if(window.location.toString().indexOf('tb=demo')!=-1){
+    //if(window.location.toString().indexOf('tb=demo')!=-1){
         for(p in $vm.module_list){
             $vm.module_list[p].Table="demo-"+$vm.module_list[p].Table;
         }
-    }
+    //}
 
 })();
